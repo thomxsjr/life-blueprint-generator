fca17d7ee6676657b41d9eaa78d29e7bf700ebb1
Thomas Nirmal <nirmalanchery@gmail.com>
2025-02-09 18:44:28 +0530

    v1.0.0
    git commit -m v1.0.0
    
    git commit -m v1.0.0

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+console.log(process.env.MONGO_URI);
+
+app.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000');
+});
+
