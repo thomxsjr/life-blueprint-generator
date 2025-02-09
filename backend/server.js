41da5c2230ef12ef047a8b4ae3ba279cac25a12e
Thomas Nirmal <nirmalanchery@gmail.com>
2025-02-09 19:29:52 +0530

    v1.0.1

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,32 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import path from "path";
+
+import { connectDB } from "./config/db.js";
+
 dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Server is ready');
-});
+const PORT = process.env.PORT || 5000;
 
-console.log(process.env.MONGO_URI);
+const __dirname = path.resolve();
+
+app.use(express.json());
+
+// app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 
 app.listen(5000, () => {
-    console.log('Server is running on http://localhost:5000');
+    connectDB();
+    console.log('Server is running on http://localhost:'+PORT);
 });
 
