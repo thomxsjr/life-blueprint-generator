be09c7c0744546f9ac0fe603eae114a2887072f0
Thomas Nirmal <nirmalanchery@gmail.com>
2025-02-22 11:31:55 +0530

    v1.2.1

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,21 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json());
 
 app.use("/auth", authRoutes);
 
 
+
+if(process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(5000, () => {
     connectDB();
