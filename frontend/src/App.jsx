931b3b93cd57357ff8e3e28c3b97e4d148b5179e
Thomas Nirmal <nirmalanchery@gmail.com>
2025-02-23 21:28:09 +0530

    v1.4.1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,21 @@
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Welcome from './pages/Welcome';
-import { useEffect } from 'react';
 import PropTypes from "prop-types";
 import { useSelector } from 'react-redux';
 
 
 function ProtectedRoute({ children }) {
-  const navigate = useNavigate();
   const { currentUser } = useSelector((state) => state.user);
 
-
-
-  useEffect(() => {
-    const fetchUser = async () => {
-
-      if(!currentUser){
-        navigate("/signin");
-        return; 
-      }
-    };
-    fetchUser();
-  }, [currentUser, navigate]);
+  if (currentUser === undefined) {
+    return <Navigate to="/signin" replace />;
+  }
+    if(currentUser === null){
+      return <Navigate to="/signin" replace />;
+    }
 
   return <>{children}</>;
 }
