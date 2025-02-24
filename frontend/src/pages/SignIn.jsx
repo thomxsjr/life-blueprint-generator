06e8cf248b9498fe37b5bacf8f8d99fc47d856a6
Thomas Nirmal <nirmalanchery@gmail.com>
2025-02-24 22:08:23 +0530

    v1.5.0

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import {
   addUser,
@@ -42,39 +42,77 @@ export default function SignIn() {
     }
   };
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input
-          type='email'
-          placeholder='email'
-          className='border p-3 rounded-lg'
-          id='email'
-          onChange={handleChange}
-        />
-        <input
-          type='password'
-          placeholder='password'
-          className='border p-3 rounded-lg'
-          id='password'
-          onChange={handleChange}
-        />
+<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+  <img
+    alt="Your Company"
+    src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+    className="mx-auto h-10 w-auto"
+  />
+  <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+    Sign in to your account
+  </h2>
+</div>
 
-        <button
-          disabled={loading}
-          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
-        >
-          {loading ? 'Loading...' : 'Sign In'}
-        </button>
-
-      </form>
-      <div className='flex gap-2 mt-5'>
-        <p>Dont have an account?</p>
-        <Link to={'/signup'}>
-          <span className='text-blue-700'>Sign up</span>
-        </Link>
+<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+  <form onSubmit={handleSubmit} className="space-y-6">
+    <div>
+      <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+        Email address
+      </label>
+      <div className="mt-2">
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={handleChange}
+          required
+          autoComplete="email"
+          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+        />
       </div>
-      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
+
+    <div>
+      <div className="flex items-center justify-between">
+        <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+          Password
+        </label>
+      </div>
+      <div className="mt-2">
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={handleChange}
+          required
+          autoComplete="current-password"
+          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+        />
+      </div>
+    </div>
+
+    <div>
+      <button
+        disabled={loading}
+        type="submit"
+        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        {loading ? 'Loading...' : 'Sign In'}
+      </button>
+    </div>
+  </form>
+
+  <p className="mt-10 text-center text-sm/6 text-gray-500">
+    Not a member?{' '}
+    <a href="/signup" className="font-semibold text-indigo-600 hover:text-indigo-500">
+      Sign Up
+    </a>
+  </p>
+  <div>
+  {error && <p className='text-red-500 mt-5'>{error}</p>}
+  </div>
+</div>
+</div>
   );
 }
