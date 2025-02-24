20833fb60babef953301e85e48d6e34e41b7bc98
Thomas Nirmal <nirmalanchery@gmail.com>
2025-02-24 23:43:39 +0530

    v1.5.1

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,37 +1,19 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { removeUser } from '../redux/user/userSlice'
+import { useSelector } from 'react-redux'
+import Header from '../components/Header'
 
 const Dashboard = () => {
-
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
   const { currentUser } = useSelector((state) => state.user)
 
-  const handleClick = async() => {
-    try {
-      const res = await fetch('/api/auth/signout', {
-        method: 'POST',
-      })
-      const data = await res.json()
-      if(data.success){
-        dispatch(removeUser())
-        navigate('/')
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  console.log(currentUser)
 
   return (
-    <div>
-      {currentUser.name}
-      <br />
-      {currentUser.email}
-      <br />
-      <button onClick={handleClick}>Logout</button>
-    </div>
+    <>
+      <Header/>
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <h1 className="text-2xl font-semibold tracking-tight text-balance text-gray-700 sm:text-4xl">
+              Welcome, {currentUser.name}! 👋
+        </h1>
+      </div>
+    </>
   )
 }
 
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -96,7 +96,7 @@ export default function SignIn() {
       <button
         disabled={loading}
         type="submit"
-        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        className="cursor-pointer flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
         {loading ? 'Loading...' : 'Sign In'}
       </button>
