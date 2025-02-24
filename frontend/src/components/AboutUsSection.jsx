b343d4d1bc4e4161c30157708ec6c979cd6e5c30
Thomas Nirmal <nirmalanchery@gmail.com>
2025-02-24 21:17:28 +0530

    v1.4.4

--- frontend/src/components/AboutUsSection.jsx
+++ frontend/src/components/AboutUsSection.jsx
@@ -3,19 +3,19 @@ const people = [
       name: 'Nirmal Kurian Thomas',
       rollno: '21137032',
       imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        '/images/nirmal-avatar.jpg',
     },
     {
         name: 'A M Tejo Sharan',
         rollno: '21137048',
         imageUrl:
-          'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+          '/images/tejo-avatar.jpg',
       },
       {
         name: 'Hirthik G',
         rollno: '21137052',
         imageUrl:
-          'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+          '/images/hirthik-avatar.jpg',
       },
   ]
   
