05b5832a1d58293370433dbc142d8a003f38ba70
Thomas Nirmal <nirmalanchery@gmail.com>
2025-02-25 17:10:40 +0530

    v1.6.1

--- frontend/src/components/CircularMetricCard.jsx
+++ frontend/src/components/CircularMetricCard.jsx
@@ -2,10 +2,10 @@ import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import PropTypes from 'prop-types';
 
-const MetricCard = ({ title, value }) => {
+const CircularMetricCard = ({ title, value }) => {
   return (
     <div className="metric-card font-bold">
-      <div className="relative w-62 h-62 mb-6">
+      <div className="relative w-62 h-62 mb-6 md:w-42 md:h-42">
         <CircularProgressbar
           value={value}
           text={`${value}`}
@@ -25,9 +25,115 @@ const MetricCard = ({ title, value }) => {
   );
 };
 
-MetricCard.propTypes = {
+CircularMetricCard.propTypes = {
     title: PropTypes.string.isRequired,
     value: PropTypes.number.isRequired,
   };
 
-export default MetricCard;
+export default CircularMetricCard;
+
+// import { TrendingUp } from "lucide-react"
+// import {
+//   Label,
+//   PolarGrid,
+//   PolarRadiusAxis,
+//   RadialBar,
+//   RadialBarChart,
+// } from "recharts"
+
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card"
+// import { ChartConfig, ChartContainer } from "@/components/ui/chart"
+// const chartData = [
+//   { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+// ]
+
+// const chartConfig = {
+//   visitors: {
+//     label: "Visitors",
+//   },
+//   safari: {
+//     label: "Safari",
+//     color: "hsl(var(--chart-2))",
+//   },
+// } satisfies ChartConfig
+
+// export default function CircularMetricCard() {
+//   return (
+//     <Card className="flex flex-col">
+//       <CardHeader className="items-center pb-0">
+//         <CardTitle>Radial Chart - Text</CardTitle>
+//         <CardDescription>January - June 2024</CardDescription>
+//       </CardHeader>
+//       <CardContent className="flex-1 pb-0">
+//         <ChartContainer
+//           config={chartConfig}
+//           className="mx-auto aspect-square max-h-[250px]"
+//         >
+//           <RadialBarChart
+//             data={chartData}
+//             startAngle={0}
+//             endAngle={250}
+//             innerRadius={80}
+//             outerRadius={110}
+//           >
+//             <PolarGrid
+//               gridType="circle"
+//               radialLines={false}
+//               stroke="none"
+//               className="first:fill-muted last:fill-background"
+//               polarRadius={[86, 74]}
+//             />
+//             <RadialBar dataKey="visitors" background cornerRadius={10} />
+//             <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+//               <Label
+//                 content={({ viewBox }) => {
+//                   if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+//                     return (
+//                       <text
+//                         x={viewBox.cx}
+//                         y={viewBox.cy}
+//                         textAnchor="middle"
+//                         dominantBaseline="middle"
+//                       >
+//                         <tspan
+//                           x={viewBox.cx}
+//                           y={viewBox.cy}
+//                           className="fill-foreground text-4xl font-bold"
+//                         >
+//                           {chartData[0].visitors.toLocaleString()}
+//                         </tspan>
+//                         <tspan
+//                           x={viewBox.cx}
+//                           y={(viewBox.cy || 0) + 24}
+//                           className="fill-muted-foreground"
+//                         >
+//                           Visitors
+//                         </tspan>
+//                       </text>
+//                     )
+//                   }
+//                 }}
+//               />
+//             </PolarRadiusAxis>
+//           </RadialBarChart>
+//         </ChartContainer>
+//       </CardContent>
+//       <CardFooter className="flex-col gap-2 text-sm">
+//         <div className="flex items-center gap-2 font-medium leading-none">
+//           Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+//         </div>
+//         <div className="leading-none text-muted-foreground">
+//           Showing total visitors for the last 6 months
+//         </div>
+//       </CardFooter>
+//     </Card>
+//   )
+// }
+
--- frontend/src/components/ScoreMetrics.jsx
+++ frontend/src/components/ScoreMetrics.jsx
@@ -33,10 +33,10 @@ const ScoreMetrics = () => {
         <section className="px-4 py-4">
             <div className="max-w-screen-xl mx-auto text-center">
                 <div className="mt-12">
-                    <ul className="grid gap-y-20 gap-x-10 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3">
+                    <ul className="grid gap-y-20 gap-x-10 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3">
                         {features.map((item, idx) => (
                             
-                            <li key={idx} className="bg-white/40 border border-white border-opacity-55 rounded-4xl p-6 flex flex-col items-center text-center hover:translate-y-[-4px] transition-all duration-300 shadow-[0_10px_20px_rgba(221,147,234,_0.3)]">
+                            <li key={idx} className="bg-white rounded-4xl p-6 flex flex-col items-center text-center hover:translate-y-[-4px] transition-all duration-300 shadow-[0_10px_20px_rgba(221,147,234,_0.3)]">
                                 <div className="flex items-center justify-center">
                                     {item.metric}
                                 </div>
