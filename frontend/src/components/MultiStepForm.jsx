3dc9cca24ee7dc3feea9b2837ffa9337de191530
Thomas Nirmal <nirmalanchery@gmail.com>
2025-02-27 19:44:19 +0530

    v2.0.3

--- frontend/src/components/MultiStepForm.jsx
+++ frontend/src/components/MultiStepForm.jsx
@@ -21,17 +21,18 @@ export default function MultiStepForm() {
         setStep(prevStep => Math.max(prevStep - 1, 1));
     };
 
+    const handleStepperClick = (step) => {
+        setStep(step)
+    }
+
 
   return (
-    <form>
-      <div className="space-y-12">
-        <div className="border-b border-gray-900/10 pb-12">
+    <>
 
-
-           <ol className="lg:flex items-center w-full space-y-4 lg:space-x-8 lg:space-y-0">
+        <ol className="lg:flex items-center w-full space-y-4 lg:space-x-8 lg:space-y-0">
       
             {stepper.map((item)=>(
-                <li key={item.step} className=" flex-1">
+                <li onClick={() => handleStepperClick(item.step)} key={item.step} id={item.step} className=" flex-1">
                     <div className={item.step==step? "border-l-2 flex flex-col border-t-0 pl-4 pt-0 border-solid border-indigo-600 font-medium lg:pt-4 lg:border-t-2 lg:border-l-0 lg:pl-0" 
                         : "border-l-2 flex flex-col border-t-0 pl-4 pt-0 border-solid border-gray-600 font-medium lg:pt-4 lg:border-t-2 lg:border-l-0 lg:pl-0"
                     }>
@@ -42,102 +43,145 @@ export default function MultiStepForm() {
                     </div>
                 </li>
             ))}
-            </ol>
+        </ol>
+    <form>
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-4">
-              <label htmlFor="username" className="block text-sm/6 font-medium text-gray-900">
-                Username
-              </label>
-              <div className="mt-2">
-                <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                  <div className="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">workcation.com/</div>
-                  <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    placeholder="janesmith"
-                    className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                  />
+            
+            {step === 1 && (
+                <>
+                <div className="sm:col-span-4">
+                <label htmlFor="username" className="block text-sm/6 font-medium text-gray-900">
+                  Username
+                </label>
+                <div className="mt-2">
+                  <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                    <div className="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">workcation.com/</div>
+                    <input
+                      id="username"
+                      name="username"
+                      type="text"
+                      placeholder="janesmith"
+                      className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
 
             <div className="col-span-full">
-              <label htmlFor="about" className="block text-sm/6 font-medium text-gray-900">
+                <label htmlFor="about" className="block text-sm/6 font-medium text-gray-900">
                 About
-              </label>
-              <div className="mt-2">
+                </label>
+                <div className="mt-2">
                 <textarea
-                  id="about"
-                  name="about"
-                  rows={3}
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  defaultValue={''}
+                    id="about"
+                    name="about"
+                    rows={3}
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    defaultValue={''}
                 />
-              </div>
-              <p className="mt-3 text-sm/6 text-gray-600">Write a few sentences about yourself.</p>
+                </div>
+                <p className="mt-3 text-sm/6 text-gray-600">Write a few sentences about yourself.</p>
             </div>
+            </>
+            ) }
 
-            <div className="sm:col-span-3">
-              <label htmlFor="first-name" className="block text-sm/6 font-medium text-gray-900">
-                First name
-              </label>
-              <div className="mt-2">
-                <input
-                  id="first-name"
-                  name="first-name"
-                  type="text"
-                  autoComplete="given-name"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
-            </div>
+            
+            {step === 2 && (
+                <>
+                    <div className="sm:col-span-3">
+                    <label htmlFor="first-name" className="block text-sm/6 font-medium text-gray-900">
+                        First name
+                    </label>
+                    <div className="mt-2">
+                        <input
+                        id="first-name"
+                        name="first-name"
+                        type="text"
+                        autoComplete="given-name"
+                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                    </div>
+                    </div>
 
-            <div className="sm:col-span-3">
-              <label htmlFor="last-name" className="block text-sm/6 font-medium text-gray-900">
-                Last name
-              </label>
-              <div className="mt-2">
-                <input
-                  id="last-name"
-                  name="last-name"
-                  type="text"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
-            </div>
+                    <div className="sm:col-span-3">
+                    <label htmlFor="last-name" className="block text-sm/6 font-medium text-gray-900">
+                        Last name
+                    </label>
+                    <div className="mt-2">
+                        <input
+                        id="last-name"
+                        name="last-name"
+                        type="text"
+                        autoComplete="family-name"
+                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                    </div>
+                    </div>
+                </>
+            )}
+            
 
+            {step === 3 && (
+                <>
+                    <div className="sm:col-span-3">
+                        <label htmlFor="country" className="block text-sm/6 font-medium text-gray-900">
+                            Country
+                        </label>
+                        <div className="mt-2 grid grid-cols-1">
+                            <select
+                            id="country"
+                            name="country"
+                            autoComplete="country-name"
+                            className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            >
+                            <option>Canada</option>
+                            <option>United States</option>
+                            <option>Mexico</option>
+                            </select>
+                            <ChevronDownIcon
+                            aria-hidden="true"
+                            className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                            />
+                        </div>
+                    </div>
+                </>
+            )}
 
-            <div className="sm:col-span-3">
-              <label htmlFor="country" className="block text-sm/6 font-medium text-gray-900">
-                Country
-              </label>
-              <div className="mt-2 grid grid-cols-1">
-                <select
-                  id="country"
-                  name="country"
-                  autoComplete="country-name"
-                  className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                >
-                  <option>United States</option>
-                  <option>Canada</option>
-                  <option>Mexico</option>
-                </select>
-                <ChevronDownIcon
-                  aria-hidden="true"
-                  className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                />
-              </div>
-            </div>
+            {step === 4 && (
+                <>
+                    <div className="sm:col-span-3">
+                        <label htmlFor="country" className="block text-sm/6 font-medium text-gray-900">
+                            Country
+                        </label>
+                        <div className="mt-2 grid grid-cols-1">
+                            <select
+                            id="country"
+                            name="country"
+                            autoComplete="country-name"
+                            className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            >
+                            <option>United States</option>
+                            <option>Canada</option>
+                            <option>Mexico</option>
+                            </select>
+                            <ChevronDownIcon
+                            aria-hidden="true"
+                            className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                            />
+                        </div>
+                    </div>
+                </>
+            )}
+            
 
           </div>
 
-          <div className="mt-10 space-y-10">
 
-
-            <fieldset>
+            {/* <fieldset>
               <legend className="text-sm/6 font-semibold text-gray-900">Push notifications</legend>
               <div className="mt-6 space-y-6">
                 <div className="flex items-center gap-x-3">
@@ -175,8 +219,7 @@ export default function MultiStepForm() {
                   </label>
                 </div>
               </div>
-            </fieldset>
-          </div>
+            </fieldset> */}
         </div>
       </div>
 
@@ -195,5 +238,6 @@ export default function MultiStepForm() {
         </button>
       </div>
     </form>
+    </>
   )
 }
